29edb388ad7023d47dff61fede40b399915d925b
Anton Kulakov <anton.kulakoff@ya.ru>
2022-05-31 23:17:28 +0300

    modify schema validator

--- src/screens/DevicesPage/validation.ts
+++ src/screens/DevicesPage/validation.ts
@@ -1,7 +1,10 @@
 import * as yup from "yup";
 
 const schema = yup.object().shape({
-  value: yup.string().min(17).max(17).required(),
+  value: yup.string().length(17,"Укажите верно MAC : AA:BB:CC:DD:EE:FF. Не более 17 символов")
+  .matches(/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/, "Укажите верно MAC : AA:BB:CC:DD:EE:FF")
+  .uppercase()
+  .required(),
   // serialNumber: yup.string().min(15).max(30).required(),
 });
 
